7587cb380f30e7bc166f8cd865a6c038517631c5
wwansterww <ansar.iken@narxoz.kz>
2025-05-16 03:38:53 +0500

    Shapes

---
+++ Game/Shapes.java
@@ -0,0 +1,17 @@
+package Game;
+
+import java.awt.*;
+
+class Shapes extends Rectangle{
+    Shapes(int x,int y,int width,int height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+    }
+
+    void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(Main.engine.scaleX(x), Main.engine.scaleY(y), Main.engine.scaleSizeX(width), Main.engine.scaleSizeY(height));
+    }
+}
