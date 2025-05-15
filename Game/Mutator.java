0f19e5e93a2922926579e0b5568c3da63ec731f7
wwansterww <ansar.iken@narxoz.kz>
2025-05-16 03:40:07 +0500

    with mutator

--- Game/Mutator.java
+++ Game/Mutator.java
@@ -1,4 +1,73 @@
 package Game;
 
-public class Mutator {
+import Engine.TimedEvent;
+
+import java.awt.*;
+
+class Mutator extends Ball{
+    MutatorT type;
+    byte count=0;
+    final Color secColor;
+    final Color firColor;
+
+    Mutator(MutatorT type){
+        color=Color.red;
+        switch(type){
+            case EXTEND_GOAL:{
+                color=Color.red;
+                firColor=Color.red;
+                secColor=Color.blue;
+                break;
+            }case SHRINK_GOAL:{
+                color=Color.blue;
+                firColor=Color.blue;
+                secColor=Color.blue;
+                break;
+            }default:{
+                color=Color.blue;
+                firColor=Color.blue;
+                secColor=Color.blue;
+            }
+        }
+        size=16;
+        radius=8;
+        this.type=type;
+        xPos=Main.rng.nextInt(600)+300;
+        yPos=Main.rng.nextInt(600)+100;
+    }
+
+
+    void move(){
+        super.move();
+    }
+    void activate(boolean left){
+        Player player;
+        if(left){
+            player=Main.player1;
+        }else{
+            player=Main.player2;
+        }
+        if(type==MutatorT.EXTEND_GOAL){
+            player.setGoalSize(player.goalsize+60);
+            new TimedEvent(24000){
+                public void run(){
+                    super.run();
+                    if(Main.mutators.contains(this))player.setGoalSize(player.goalsize-60);}
+            }.start();
+        }else if(type==MutatorT.SHRINK_GOAL){
+            player.setGoalSize(player.goalsize-40);
+            new TimedEvent(20000){
+                public void run(){
+                    super.run();
+                    if(Main.mutators.contains(this))player.setGoalSize(player.goalsize+40);}
+            }.start();
+        }
+        Main.mutatorsToRemove.add(this);
+    }
+
+}
+
+enum MutatorT{
+    EXTEND_GOAL,
+    SHRINK_GOAL,
 }
